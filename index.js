cfb312443f1796df40ea06b7306566b722f0c253
Newland-A <ajn252@gmail.com>
2020-11-20 12:08:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function addingEventListener() {
+function addingEventListener('onClick', event) {
   const input = document.getElementById('input');
 input.addEventListener('click', function(event) {
   alert('I was clicked!');
